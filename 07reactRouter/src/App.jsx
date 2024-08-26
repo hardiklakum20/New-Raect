441d49a4f1dc539345be7f1b67ea6c8b86e7bd53
hardiklakum20 <120251398+hardiklakum20@users.noreply.github.com>
2024-08-26 15:47:39 +0530

    react router dom

---
+++ 07reactRouter/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Footer from './componets/Footer/Footer'
+import Header from './componets/Header/Header'
+import Home from './componets/Home/Home'
+
+function App() {  
+
+  return (
+    <>     
+    </>
+  )
+}
+
+export default App
