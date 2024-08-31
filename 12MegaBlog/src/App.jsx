777fc54e1a4d2bfb93f772fd1e8c536dd2e3fd80
hardiklakum20 <120251398+hardiklakum20@users.noreply.github.com>
2024-08-31 15:20:22 +0530

    making store in megablog

--- 12MegaBlog/src/App.jsx
+++ 12MegaBlog/src/App.jsx
@@ -1,14 +1,40 @@
+import { useEffect, useState } from 'react'
+import {useDispatch} from 'react-redux'
 import './App.css'
+import authService from './appWrite/auth'
+import {login, logout} from './store/authSlice'
+import { Footer, Header } from './componets'
 
 function App() {
   
-  console.log(import.meta.env.VITE_APPWRITE_URL);
+  const [loading, setLoading] = useState(true)
+
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    authService.getCurrentUser()
+    .then((userData) => {
+        if(userData){
+          dispatch(login({userData}))
+        }else{
+          dispatch(logout())
+        }
+    })
+    .finally(() => setLoading(false))
+
+  },[])
   
-  return (
-    <>
-      <h1>A blog app with appwrite</h1>
-    </>
-  )
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header/>
+        <main>
+           Todo: {/* <Outlet/> */}
+        </main>
+        <Footer/>
+      </div>
+    </div>
+  ) : (null)
 }
 
 export default App
